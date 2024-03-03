d3c3e196fb82276c45acc5a8d154786dfe4acb09
rahullrawat <rahulsinghrawat760@gmail.com>
2024-03-03 19:26:37 +0530

    feat(courses): add courses data table page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,27 @@
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
+import { redirect } from 'next/navigation'
+import { auth } from '@clerk/nextjs'
+import { DataTable } from './_components/data-table'
+import { columns } from './_components/columns'
+import { db } from '@/lib/db'
 
-export default function Courses() {
+export default async function Courses() {
+  const { userId } = auth()
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      createdById: userId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>Create Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   )
 }
