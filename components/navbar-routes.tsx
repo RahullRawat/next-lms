ab7b0a0d32f88e3ad607040dcd3847cb1d8bb796
rahullrawat <rahulsinghrawat760@gmail.com>
2024-06-23 15:36:53 +0530

    feat(course): add courses chapter layout

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,18 +1,22 @@
 'use client'
 
-import { UserButton } from '@clerk/nextjs'
-import Link from 'next/link'
+import { UserButton, useAuth } from '@clerk/nextjs'
 import { usePathname } from 'next/navigation'
 import { LogOut } from 'lucide-react'
-import { Button } from './ui/button'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
 import { SearchInput } from './search-input'
+import { isTeacher } from '@/lib/teacher'
 
-export default function NavbarRoutes() {
-  const pathName = usePathname()
+export const NavbarRoutes = () => {
+  const { userId } = useAuth()
 
-  const isTeacherRoute = pathName?.startsWith('teacher')
-  const isPlayerRoute = pathName?.includes('teacher')
-  const isSearchPage = pathName === '/search'
+  const pathname = usePathname()
+
+  const isTeacherPage = pathname?.startsWith('/teacher')
+  const isCoursePage = pathname?.includes('/courses')
+  const isSearchPage = pathname?.includes('/search')
 
   return (
     <>
@@ -21,21 +25,21 @@ export default function NavbarRoutes() {
           <SearchInput />
         </div>
       )}
-      <div className="ml-auto flex items-center gap-x-2">
-        {isTeacherRoute || isPlayerRoute ? (
+      <div className="ml-auto flex gap-x-2">
+        {isTeacherPage || isCoursePage ? (
           <Link href="/">
             <Button size="sm" variant="ghost">
               <LogOut className="mr-2 h-4 w-4" />
               Exit
             </Button>
           </Link>
-        ) : (
+        ) : isTeacher(userId) ? (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
-              Teacher Mode
+              Teacher mode
             </Button>
           </Link>
-        )}
+        ) : null}
         <UserButton afterSignOutUrl="/" />
       </div>
     </>
