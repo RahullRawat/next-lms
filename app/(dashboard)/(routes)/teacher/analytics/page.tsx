f2009dfc1900116d193548ae64f368a4b99703b0
rahullrawat <rahulsinghrawat760@gmail.com>
2024-07-10 23:55:00 +0530

    feat: add teacher analytics dashboard

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,3 +1,25 @@
-export default function Analytics() {
-  return <div>Analytics Page!</div>
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { getAnalytics } from '@/actions/get-analytics'
+import DataCard from './_components/data-card'
+
+export default async function Analytics() {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId)
+
+  return (
+    <div className="p-6">
+      <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+
+      <Chart data={data} />
+    </div>
+  )
 }
